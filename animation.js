a046b807585476ac9765139bc8873a0fb3691486
Yang <320035139@qq.com>
2022-04-14 00:21:49 +0800

    跳动的爱心

---
+++ animation.js
@@ -0,0 +1,16 @@
+// onclick = () => {
+//   demo.classList.add("end");
+//   setTimeout(() => {
+//     demo.remove();
+//   }, 1000);
+// };
+// onclick = () => {
+//   demo.classList.add("b");
+//   setTimeout(() => {
+//     demo.classList.remove("b");
+//     demo.classList.add("c");
+//   }, 1000);
+// };
+// onclick = () => {
+//   demo.classList.add("start");
+// };
